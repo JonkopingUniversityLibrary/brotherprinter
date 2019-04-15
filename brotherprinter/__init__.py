6a9fca624c2a454a021285d9580da3085b109a06
Gustav Lindqvist <gustav.lindqvist@ju.se>
2019-04-15 08:58:55 +0200

    Added changes from translation branch

--- brotherprinter/__init__.py
+++ brotherprinter/__init__.py
@@ -7,61 +7,72 @@ class BrotherPrinter:
         self.constants = {
             'bpoHighResolution': 0x02000000
         }
+        self.default_printer = None
 
     def get_printer_info(self, bpac):
-        for printer in bpac.Printer.GetInstalledPrinters:
+        printer = self.default_printer
+        if printer:
             support = True if bpac.Printer.IsPrinterSupported(printer) else False
-            status = "Online" if bpac.Printer.IsPrinterOnline(printer) else "Offline"
+            online = True if bpac.Printer.IsPrinterOnline(printer) else False
             return {
                 'printer_info': {
                     'name': printer,
                     'is_supported': support,
-                    'status': status
+                    'online': online
                 }
             }
+        else:
+            return None
 
     def get_label_info(self, bpac):
-        for printer in bpac.Printer.GetInstalledPrinters:
-            bpac.SetPrinter(printer, False)
-
-            id = bpac.Printer.GetMediaId
-            name = bpac.Printer.GetMediaName
-            return {
-                'label_info': {
-                    'id': id,
-                    'name': name if name else "No Media"
-                }
+        printer = self.default_printer
+        bpac.SetPrinter(printer, False)
+        id = bpac.Printer.GetMediaId
+        name = bpac.Printer.GetMediaName
+        return {
+            'label_info': {
+                'id': id,
+                'name': name if name else "No media"
             }
+        }
 
-    def get_available_printer(self, bpac):
+    @staticmethod
+    def get_available_printer(bpac):
         printers = bpac.Printer.GetInstalledPrinters
         for printer in printers:
-            if bpac.Printer.IsPrinterSupported(printer) \
-                    and bpac.Printer.IsPrinterOnline(printer):
+            if bpac.Printer.IsPrinterSupported(printer) and \
+                    bpac.Printer.IsPrinterOnline(printer):
                 return printer
         else:
             return None
 
-    def print_callnumber(self, bpac, *, template, call_number):
-        printer = self.get_available_printer(bpac)
+    def set_default_printer(self, bpac):
+        self.default_printer = self.get_available_printer(bpac)
+
+    @staticmethod
+    def get_printers(bpac):
+        return bpac.Printer.GetInstalledPrinters
+
+    def print_callnumber(self, bpac, *, template, call_number, printer=None):
+        printer = printer if printer else self.default_printer
         if not printer:
-            raise PrintException('No printers available')
+            raise PrintException('No printer available')
 
         has_opened = bpac.Open(template)
         if not has_opened:
-            raise PrintException('No template available at the path "' + template + '"')
+            raise PrintException('The template on the path "' + template + '" is not available')
 
         if not bpac.StartPrint("", self.constants['bpoHighResolution']):
-            raise PrintException('Failed to start print')
+            raise PrintException('Failed to start print job')
 
         bpac.GetObject("CallNumber").Text = call_number
         PrintOut = bpac.PrintOut(1, self.constants['bpoHighResolution'])
 
         if not PrintOut:
-            raise PrintException('Failed to Print Out (ErrorCode ' + bpac.ErrorCode + ')')
+            raise PrintException('failed to print out (Errorcode ' + bpac.ErrorCode + ')')
         if not bpac.EndPrint:
-            raise PrintException('Failed to end print')
+            raise PrintException('Failed to end print job')
         if not bpac.Close:
-            raise PrintException('Failed to close print')
+            raise PrintException('Failed to close printer connection')
 
         return True
